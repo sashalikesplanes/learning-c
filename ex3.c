203170fdeab29c4a0c3d4bcd6f66d19f5e45fa58
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-10-03 11:43:23 +0200

    completed some exercises

---
+++ ex3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int age = 10;
+  int height = 72;
+
+  printf("I am %d years old.\n", age);
+  printf("I am %d inches tall.\n", height);
+
+  if (argc == 1)
+  {
+    printf("no beep\n");
+  }
+  else 
+  {
+    printf("beeeeep \a \n");
+  }
+
+  return 0;
+}
